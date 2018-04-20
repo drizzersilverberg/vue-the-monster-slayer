891c250850f2088cb1dd91cdf5f649df66b913c7
Dendi 'Drizzer' Handian <dendihandian@gmail.com>
2018-04-20 23:28:20 +0700

    implementing special attack

--- js/app.js
+++ js/app.js
@@ -13,16 +13,19 @@ new Vue ({
     },
     attack: function() {
       this.monsterHealth -= this.calculateDamage(3, 10);
-
       if (this.checkWin()) {
         return;
       }
 
-      this.playerHealth -= this.calculateDamage(5, 12);
-      this.checkWin();
+      this.monsterAttack();
     },
     specialAttack: function() {
+        this.monsterHealth -= this.calculateDamage(10, 20);
+        if (this.checkWin()) {
+          return;
+        }
 
+        this.monsterAttack();
     },
     heal: function() {
 
@@ -30,11 +33,15 @@ new Vue ({
     giveUp: function() {
 
     },
+    monsterAttack: function() {
+      this.playerHealth -= this.calculateDamage(5, 12);
+      this.checkWin();
+    },
     calculateDamage: function(min, max) {
         return Math.max(Math.floor(Math.random() * max) + 1, min);
     },
     checkWin: function() {
-      if (this.monsterHuealth <= 0) {
+      if (this.monsterHealth <= 0) {
         if (confirm('You won! New Game?')) {
           this.startGame();
         } else {
